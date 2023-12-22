3f5676a9ce1375802d3598bd0012d96ced413c90
Yurii Mezentsev <yvmezentsev@gmail.com>
2023-12-22 17:17:07 +0200

    Add Security

--- src/main/java/com/example/hw12springcore/config/WebSecurityConfig.java
+++ src/main/java/com/example/hw12springcore/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 @EnableMethodSecurity
 public class WebSecurityConfig {
